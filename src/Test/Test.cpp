f94703deb7a42fc8ece67cd94e02efa8d9220bc8
Andrey Drogolyub <andrey.drogolyub@altezza.org>
2026-01-15 18:10:41 +0300

    add tests

--- src/Test/Test.cpp
+++ src/Test/Test.cpp
@@ -53,6 +53,10 @@ namespace Test
     bool name##AddToList(){ g_groups.push_back(Group(#name, name##Test)); return true; } \
     bool name##AtList = name##AddToList();
 
+    TEST_ADD(HasArg);
+    TEST_ADD(HasArg1);
+    TEST_ADD(HasArg2);
+
     TEST_ADD(LogCallback);
     TEST_ADD(LogCallbackRaw);
     TEST_ADD(LogDateTime);
