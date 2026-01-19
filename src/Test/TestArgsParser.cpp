eff0ee15388ae0bd745b459ca4779c29393194cd
adrogolyub <adrogolub@gmail.com>
2026-01-19 11:47:45 +0300

    Fix hasarg (#22)
    * add tests
    
    * fix infinite recursion
    
    ---------
    
    Co-authored-by: Andrey Drogolyub <andrey.drogolyub@altezza.org>

---
+++ src/Test/TestArgsParser.cpp
@@ -0,0 +1,73 @@
+/*
+* Tests for Common Purpose Library (http://github.com/ermig1979/Cpl).
+*
+* Copyright (c) 2021-2022 Yermalayeu Ihar,
+*               2021-2022 Andrey Drogolyub.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+#include "Test/Test.h"
+
+#include "Cpl/Args.h"
+
+namespace Test
+{
+    struct TestArgs : public Cpl::ArgsParser
+    {
+        TestArgs(int argc, char* argv[])
+            : Cpl::ArgsParser(argc, argv, true) {}
+
+        bool Check(const Cpl::String& name) { return HasArg(name); }
+        bool Check(const String& name0, const String& name1) { return HasArg(name0, name1); }
+    };
+
+    bool HasArgTest()
+    {
+        int argc = 2;
+        std::vector<char*> args = {"test", "-h"};
+
+        TestArgs a(argc, args.data());
+
+        return a.Check("-h");
+    }   
+
+    bool HasArg1Test()
+    {
+        int argc = 2;
+        std::vector<char*> args = { "test", "-h" };
+
+        TestArgs a(argc, args.data());
+
+        return !a.Check("-g");
+    }
+
+    bool HasArg2Test()
+    {
+        int argc = 2;
+        std::vector<char*> args = { "test", "-h" };
+
+        TestArgs a(argc, args.data());
+
+        return a.Check("-h", "-?");
+    }
+}
+
+
+
